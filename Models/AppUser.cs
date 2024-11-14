efb083133dc21abe0e94da8ec1839d4441543e1f
abmah <ab.mahasnh@gmail.com>
2024-11-14 22:11:37 +0300

    'new'

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -9,5 +9,8 @@ namespace api.Models
     public class AppUser : IdentityUser
     {
         // IdentityUser already has what we need inside it 
+
+
+        public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
     }
 }
--- Models/Comment.cs
+++ Models/Comment.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace api.Models
 {
+
+    [Table("Comments")]
     public class Comment
     {
         public int Id { get; set; }
